bada37d9bb0cb9b57576d6fb467e840ecc86e587
Oskar Höstlund <oskarhostlund@gmail.com>
2025-05-05 15:49:15 +0200

    init

---
+++ src/main/java/com/hostlund/snus/SnusApplication.java
@@ -0,0 +1,13 @@
+package com.hostlund.snus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SnusApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hostlund/snus/SnusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostlund.snus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnusApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
