fbe043e80ec1c31407a7d994385b99d9ab2c5a07
Oskar Höstlund <oskarhostlund@gmail.com>
2025-05-06 23:09:50 +0200

    Get snus by Id, refactoring paths

--- src/main/java/com/hostlund/snus/controller/SnusController.java
+++ src/main/java/com/hostlund/snus/controller/SnusController.java
@@ -5,20 +5,24 @@ import com.hostlund.snus.services.SnusService;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/snus")
 public class SnusController {
 
   private final SnusService snusService;
 
-  public Snus getSnusById(UUID id) {
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public Snus getSnusById(@PathVariable("id") UUID id) {
     return snusService.getSnusById(id);
   }
 
-  @RequestMapping("/api/v1/snus")
+  @RequestMapping(method = RequestMethod.GET)
   public List<Snus> getSnus() {
     return snusService.listSnus();
   }
