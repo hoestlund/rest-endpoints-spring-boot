7f17f0c8a125bc96e90dda92aed05a159baf2eb9
Oskar Höstlund <oskarhostlund@gmail.com>
2025-05-08 02:54:17 +0200

    Implemented customer controller and service

---
+++ src/main/java/com/hostlund/snus/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package com.hostlund.snus.controller;
+
+import com.hostlund.snus.model.Customer;
+import com.hostlund.snus.model.Snus;
+import com.hostlund.snus.services.CustomerService;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+  private final CustomerService customerService;
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public Customer getCustomerById(@PathVariable("id") UUID id) {
+    return customerService.getCustomer(id);
+  }
+
+  @RequestMapping(method = RequestMethod.GET)
+  public List<Customer> getCustomers() {
+    return customerService.getCustomers();
+  }
+
+}
---
+++ src/main/java/com/hostlund/snus/services/CustomerServiceImpl.java
@@ -0,0 +1,60 @@
+package com.hostlund.snus.services;
+
+import com.hostlund.snus.model.Address;
+import com.hostlund.snus.model.Customer;
+import com.neovisionaries.i18n.CountryCode;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+  private Map<UUID,Customer> customerMap;
+
+  public CustomerServiceImpl() {
+    this.customerMap = new HashMap<>();
+
+    Customer customer_1 = Customer.builder()
+        .id(UUID.randomUUID())
+        .firstName("Laila")
+        .lastName("Ross")
+        .email("laila.ross@gmail.com")
+        .address(Address.builder()
+            .firstLine("Puttbusser Str")
+            .houseNumber(8)
+            .city("Berlin")
+            .cc(CountryCode.DE)
+            .build())
+        .build();
+
+    Customer customer_2 = Customer.builder()
+        .id(UUID.randomUUID())
+        .firstName("Jeremy")
+        .lastName("Walton")
+        .email("jwally@gmail.com")
+        .address(Address.builder()
+            .firstLine("Högsbogatan")
+            .houseNumber(23)
+            .city("Gothenburg")
+            .cc(CountryCode.SE)
+            .build())
+        .build();
+
+    customerMap.put(customer_1.getId(),customer_1);
+    customerMap.put(customer_2.getId(),customer_2);
+  }
+
+  @Override
+  public Customer getCustomer(UUID id) {
+    return customerMap.get(id);
+  }
+
+  @Override
+  public List<Customer> getCustomers() {
+    return new ArrayList<>(customerMap.values());
+  }
+}
