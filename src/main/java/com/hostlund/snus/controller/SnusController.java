f50928357bcb5f46378d0b4eae50485994f0fad0
Oskar Höstlund <oskarhostlund@gmail.com>
2025-10-20 17:31:10 +0200

    Using WebMvcTest
    json returns an empty list

--- src/main/java/com/hostlund/snus/controller/SnusController.java
+++ src/main/java/com/hostlund/snus/controller/SnusController.java
@@ -8,13 +8,11 @@ import com.hostlund.snus.services.SnusService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.net.URI;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.HeadersBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,65 +21,64 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/snus")
 public class SnusController {
 
-  private final SnusService snusService;
+    private final SnusService snusService;
 
-  @PostMapping
-  public ResponseEntity<SnusDTO> handlePost(@Valid @RequestBody SnusDTO snus,
-      UriComponentsBuilder uriBuilder) {
-    Snus savedSnus = snusService.saveSnus(DTOToSnus(snus));
-    URI location = uriBuilder.path("api/v1/snus/{id}")
-        .buildAndExpand(savedSnus.getId())
-        .toUri();
-    return ResponseEntity.created(location).build();
-  }
-
-  @GetMapping(value = "/{id}")
-  public Snus getSnusById(@PathVariable("id") UUID id) {
-    return snusService.getSnusById(id);
-  }
-
-  @GetMapping
-  public List<Snus> getSnus() {
-    return snusService.listSnus();
-  }
-
-  @PutMapping(value = "/{id}")
-  public ResponseEntity updateSnus(@PathVariable UUID id, @RequestBody @NotNull SnusDTO snus) {
-    try {
-      //Change the update fields, version etc.
-      snusService.updateSnus(id, DTOToSnus(snus));
-    } catch (ResponseStatusException e) {
-      return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
-    return new ResponseEntity(HttpStatus.OK);
-  }
-
-  @DeleteMapping(value = "/{id}")
-  public ResponseEntity deleteSnus(@PathVariable UUID id) {
-    try {
-      snusService.deleteSnusById(id);
-    } catch (ResponseStatusException e) {
-      return new ResponseEntity(HttpStatus.NOT_FOUND);
+    @PostMapping
+    public ResponseEntity<SnusDTO> handlePost(@Valid @RequestBody SnusDTO snus,
+        UriComponentsBuilder uriBuilder) {
+        Snus savedSnus = snusService.saveSnus(DTOToSnus(snus));
+        URI location = uriBuilder.path("api/v1/snus/{id}")
+            .buildAndExpand(savedSnus.getId())
+            .toUri();
+        return ResponseEntity.created(location).build();
     }
 
-    return new ResponseEntity(HttpStatus.NO_CONTENT);
-  }
+    @GetMapping(value = "/{id}")
+    public Snus getSnusById(@PathVariable("id") UUID id) {
+        return snusService.getSnusById(id);
+    }
 
-  @PatchMapping(value = "/{id}")
-  public ResponseEntity patchSnus(@PathVariable UUID id, @RequestBody @NotNull SnusDTO snus) {
+    @GetMapping
+    public List<Snus> getSnus() {
+        return snusService.listSnus();
+    }
 
-    snusService.patchSnus(id, DTOToSnus(snus));
-    return ResponseEntity.noContent().build();
-  }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity updateSnus(@PathVariable UUID id, @RequestBody @NotNull SnusDTO snus) {
+        try {
+            //Change the update fields, version etc.
+            snusService.updateSnus(id, DTOToSnus(snus));
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity deleteSnus(@PathVariable UUID id) {
+        try {
+            snusService.deleteSnusById(id);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity patchSnus(@PathVariable UUID id, @RequestBody @NotNull SnusDTO snus) {
+
+        snusService.patchSnus(id, DTOToSnus(snus));
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/test/java/com/hostlund/snus/controller/SnusControllerTest.java
+++ src/test/java/com/hostlund/snus/controller/SnusControllerTest.java
@@ -1,24 +1,41 @@
 package com.hostlund.snus.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.core.Is.is;
 
-import com.hostlund.snus.model.Snus;
-import java.util.List;
+import com.hostlund.snus.services.SnusService;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@WebMvcTest(SnusController.class)
 class SnusControllerTest {
 
-  @Autowired
-  SnusController controller;
+    @Autowired
+    MockMvc mockMVC;
 
-  @Test
-  void getSnus() {
-    List<Snus> snus = controller.getSnus();
-    assertNotNull(snus);
-    assertNotEquals(0, snus.size());
-  }
+    @MockitoBean
+    SnusService snusService;
+
+    @Test
+    void getSnusById() throws Exception {
+        mockMVC.perform(get("/api/v1/snus/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void getSnus() throws Exception {
+        mockMVC.perform(get("/api/v1/snus").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+            //.andExpect(jsonPath("$[0].name", is("Apres N°7 Very Berry")))
+            //.andExpect(jsonPath("$.length()", is(3)));
+    }
 }
